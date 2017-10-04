e7412a3ca7bfbf3e87a697d9051fef6e1f60f14c
MabellaPortillo <mabel_abbey@outlook.es>
2017-10-03 21:05:58 -0600

    segunda

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use Mail;
+use DB;
+use Session;
+use Redirect;
+use Crypt;
+use App\User;
+
+class LoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if (Auth::attempt(['name'=>$request['name'],'password'=>$request['password']])) {
+          return view('fondo');
+        } else {
+          return redirect('/');
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+    public function logout(){
+      Auth::logout();
+      return Redirect('/');
+    }
+}
---
+++ app/User.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nombre', 'apellido', 'direccion','telefono','dui','foto','name','email','password','acceso'];
+
+    public static function buscar($nombre){
+      return User::nombre($nombre)->orderBy('apellido')->paginate(8);
+    }
+
+    public function scopeNombre($query, $nombre){
+      if(trim($nombre)!=""){
+        $query->where('nombre', 'ilike','%'.$nombre.'%')->orWhere('apellido','ilike','%'.$nombre.'%');
+      }
+    }
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
