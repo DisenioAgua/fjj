d4a63fbf12bba68aa1e5adf27a0a8cef038673e7
MabellaPortillo <mabel_abbey@outlook.es>
2017-10-03 21:08:18 -0600

    tercera

---
+++ database/migrations/2017_09_28_193202_Empleado.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Empleado extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('direccion');
+            $table->string('telefono');
+            $table->string('dui');
+            $table->string('foto');
+            $table->Integer('acceso');
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
