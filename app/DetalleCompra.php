e106e986aa4f0e0b3607a1d3ae7765cf98f390ca
MabellaPortillo <mabel_abbey@outlook.es>
2017-10-03 21:04:36 -0600

    inicio

---
+++ app/DetalleCompra.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DetalleCompra extends Model
+{
+    //
+      protected $fillable =['compra_id','producto_id','cantidad','precio'];
+}
